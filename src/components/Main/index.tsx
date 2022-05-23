3d17f3ae73d958259d2bcf56b45ffb9ad461b8c6
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2022-05-23 18:33:33 -0300

    create project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,10 +2,10 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Wrapper>
-    <S.Title>Boilerplate</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
+    <S.Title>Finances GO</S.Title>
+
+    <S.Description>Gerencie suas finanças pessoais</S.Description>
+
     <S.Illustration
       src="hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
